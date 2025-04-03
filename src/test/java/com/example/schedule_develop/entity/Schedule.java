c1c8bc011e1545a3062af2dd59f312e9fdcc7b90
escomputer <nujest307@naver.com>
2025-04-04 03:13:51 +0900

    lv 1 entity

--- src/test/java/com/example/schedule_develop/entity/Schedule.java
+++ src/test/java/com/example/schedule_develop/entity/Schedule.java
@@ -1,4 +1,36 @@
 package com.example.schedule_develop.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.catalina.User;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy  = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne@JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    private String title;
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+
 }
