a0758eb69488bd28702aa7fd30cf23053cbb3690
escomputer <nujest307@naver.com>
2025-04-04 03:55:36 +0900

    lv 1 ScheduleRequestDto init

--- src/test/java/com/example/schedule_develop/dto/ScheduleRequestDto.java
+++ src/test/java/com/example/schedule_develop/dto/ScheduleRequestDto.java
@@ -1,4 +1,21 @@
 package com.example.schedule_develop.dto;
 
+import com.example.schedule_develop.entity.Schedule;
+import com.example.schedule_develop.entity.User;
+import lombok.Getter;
+
+@Getter
 public class ScheduleRequestDto {
+    private String title;
+    private String content;
+    private Long userId;
+
+    public Schedule toEntity(User user) {
+        Schedule schedule = new Schedule();
+        schedule.setTitle(title);
+        schedule.setContent(content);
+        schedule.setUser(user);
+        return schedule;
+
+    }
 }
