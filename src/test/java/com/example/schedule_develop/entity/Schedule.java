ed8b66e11683aa5ca8a52c506b71b7737deff631
escomputer <nujest307@naver.com>
2025-04-04 04:34:44 +0900

    Fix : EntityListners 어노테이션 추가
    Auditing을 위한,,

--- src/test/java/com/example/schedule_develop/entity/Schedule.java
+++ src/test/java/com/example/schedule_develop/entity/Schedule.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import org.apache.catalina.User;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-
+@EntityListeners(AuditingEntityListener.class)
 public class Schedule {
     @Id
     @GeneratedValue(strategy  = GenerationType.IDENTITY)
