0fbff492e5514cbf89a9c80f25cc1935412942bc
escomputer <nujest307@naver.com>
2025-04-04 15:42:07 +0900

    Docs : Dto(User)
    유저로부터 요청, 유저에게 응답할 데이터

--- src/main/java/com/example/schedule_develop/dto/UserRequestDto.java
+++ src/main/java/com/example/schedule_develop/dto/UserRequestDto.java
@@ -1,4 +1,17 @@
 package com.example.schedule_develop.dto;
 
+import com.example.schedule_develop.entity.User;
+import lombok.Getter;
+
+@Getter
 public class UserRequestDto {
+    private String username;
+    private String email;
+
+    public User toEntity() {
+        User user = new User();
+        user.setUsername(this.username);
+        user.setEmail(this.email);
+        return user;
+    }
 }
--- src/main/java/com/example/schedule_develop/dto/UserResponseDto.java
+++ src/main/java/com/example/schedule_develop/dto/UserResponseDto.java
@@ -1,4 +1,25 @@
 package com.example.schedule_develop.dto;
 
+import com.example.schedule_develop.entity.User;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class UserResponseDto {
+    private String username;
+    private String email;
+    private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public UserResponseDto(User user){
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.id = user.getId();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+
+    }
+
 }
