32fddb4b58a8b72df37da8e6bed3403c80e24cb9
escomputer <nujest307@naver.com>
2025-04-05 02:58:22 +0900

    Fix : JpaRepository 상속
    ScheduleRepository도 구현체를 지움

--- src/main/java/com/example/schedule_develop/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedule_develop/repository/ScheduleRepository.java
@@ -1,12 +1,13 @@
 package com.example.schedule_develop.repository;
 
 import com.example.schedule_develop.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 
-public interface ScheduleRepository {
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     Schedule save(Schedule schedule) ;
 
     List<Schedule> findAll();
