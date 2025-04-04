794b66a334bd786388ae9dbac1823439bfaee2f5
escomputer <nujest307@naver.com>
2025-04-04 13:38:26 +0900

    Docs : User init

---
+++ src/main/java/com/example/schedule_develop/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.schedule_develop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User extends Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+
+}
