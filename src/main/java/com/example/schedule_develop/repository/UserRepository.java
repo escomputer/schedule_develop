46e2789ab93c045b91a6787fa51e255541c18be5
escomputer <nujest307@naver.com>
2025-04-04 17:53:28 +0900

    Fix : repository구현체 삭제
    jparepository를 상속 받으면서,
    기능들이 겹치기때문에 따로 구현하지않아도 된다.

--- src/main/java/com/example/schedule_develop/repository/UserRepository.java
+++ src/main/java/com/example/schedule_develop/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package com.example.schedule_develop.repository;
 
 import com.example.schedule_develop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
     Optional<User> findById(Long id);
     User save(User user);
     void deleteById(Long id);
+    Optional<User> findByEmail(String email);
+
 }
