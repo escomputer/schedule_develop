889d12d8d9688f112e2eb03169a1b8bf30dc95e9
escomputer <nujest307@naver.com>
2025-04-04 15:29:09 +0900

    Docs : UserRepository 생성

---
+++ src/main/java/com/example/schedule_develop/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.example.schedule_develop.repository;
+
+public interface UserRepository {
+}
---
+++ src/main/java/com/example/schedule_develop/repository/UserRepositoryImpl.java
@@ -0,0 +1,4 @@
+package com.example.schedule_develop.repository;
+
+public class UserRepositoryImpl implements UserRepository {
+}
