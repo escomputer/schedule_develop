c7cdd912b5b16c0b0d0e8e91e4851b0fac71f2ad
escomputer <nujest307@naver.com>
2025-04-04 15:41:44 +0900

    Docs : 단어 통일
    userName -> username 으로 다 통일

--- src/main/java/com/example/schedule_develop/dto/ScheduleResponseDto.java
+++ src/main/java/com/example/schedule_develop/dto/ScheduleResponseDto.java
@@ -10,7 +10,7 @@ public class ScheduleResponseDto {
     private Long id;
     private String title;
     private String content;
-    private String userName;
+    private String username;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
@@ -18,7 +18,7 @@ public class ScheduleResponseDto {
         this.id = schedule.getId();
         this.title = schedule.getTitle();
         this.content = schedule.getContent();
-        this.userName = schedule.getUser().getUsername();
+        this.username = schedule.getUser().getUsername();
         this.createdAt = schedule.getCreatedAt();
         this.updatedAt = schedule.getUpdatedAt();
     }
