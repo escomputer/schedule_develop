f1c6544997e79ef883f2f441820e7dda50eb6eac
escomputer <nujest307@naver.com>
2025-04-05 02:08:46 +0900

    Feat : 로그인 시 이메일과 비밀번호가 일치하지 않을 경우 HTTP Status code 401을 반환

---
+++ src/main/java/com/example/schedule_develop/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.schedule_develop;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String,Object>> illegalArgumentException(IllegalArgumentException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.UNAUTHORIZED.value());
+        response.put("error", "UNAUTHORIZED");
+        response.put("message", ex.getMessage());
+        response.put("path", "/api/auth/login");
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+
+    }
+}
